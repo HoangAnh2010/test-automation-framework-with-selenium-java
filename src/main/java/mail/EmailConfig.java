a701ac094c6f84349f7e91f16645f91daaefef55
Hoang Anh Oriole <74035444+HoangAnh2010@users.noreply.github.com>
2023-07-01 22:36:03 +0700

    Update EmailConfig.java

--- src/main/java/mail/EmailConfig.java
+++ src/main/java/mail/EmailConfig.java
@@ -1,8 +1,3 @@
-/*
- * Copyright (c) 2022.
- * Automation Framework Selenium - Anh Tester
- */
-
 package mail;
 
 import static constants.FrwConstants.REPORT_TITLE;
@@ -15,7 +10,7 @@ public class EmailConfig {
     public static final String PORT = "587";
 
     public static final String FROM = "hoanganh04092001@gmail.com";
-    public static final String PASSWORD = "dtxjkgkbfkfsiuht";
+    public static final String PASSWORD = "";
 
     public static final String[] TO = {"hoanganhoriole2010@gmail.com"};
     public static final String SUBJECT = REPORT_TITLE;
